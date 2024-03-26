1961150a3b5593528ad3478a47bdf172b6940579
Robin Barta <150230392+RobinBarta@users.noreply.github.com>
2024-03-26 15:39:49 +0100

    Update makeData_functions.py

--- data/makeData/makeData_functions.py
+++ data/makeData/makeData_functions.py
@@ -81,7 +81,7 @@ def GenerateMarker(cam,ax,ay,params):
         img = cv2.addWeighted( img, 20, img, 0, 0)  
         cv2.imwrite(params.path_output_marker.format(cam=cam,plane=str(plane+1),time=str(1).zfill(params.zFill),Number=int(params.N_particles),times=int(params.freq)),img)
         # create output data
-        data = np.append(data,np.vstack([np.round(x,1),np.round(y,1),X,Y,Z]).T,axis=0)
+        data = np.append(data,np.vstack([np.round(x,5),np.round(y,5),X,Y,Z]).T,axis=0)
     np.savetxt(params.path_output.format(Number=int(params.N_particles),times=int(params.freq))+'/calibration_images/markerlist_c'+str(cam)+'.txt',data,header='x,y,X,Y,Z')
     return 0
 
